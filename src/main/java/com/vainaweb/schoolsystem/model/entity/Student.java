4815d493fd1a61c6562b70e2abf75d304b99c2f6
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-04-05 00:17:46 -0300

    feat: Cria entidade Student e enum Course
    Este commit adiciona a entidade Student e o enum Course ao projeto. A entidade Student representa um estudante no sistema, e o enum Course representa os possíveis cursos que um estudante pode estar matriculado.
    
    - Adiciona a classe Student com atributos básicos
    - Adiciona o enum Course com opções de cursos

---
+++ src/main/java/com/vainaweb/schoolsystem/model/entity/Student.java
@@ -0,0 +1,59 @@
+package com.vainaweb.schoolsystem.model.entity;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_student")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Student {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotBlank
+  private String name;
+
+  @Email
+  @NotBlank
+  @Column(unique = true)
+  private String email;
+
+  @NotBlank
+  @CPF
+  @Column(unique = true, length = 14)
+  private String cpf;
+
+  @Pattern(regexp = "^\\(\\d{2}\\) (9\\d{4}-\\d{4}|\\d{4}-\\d{4})$")
+  private String phone;
+
+  @NotNull
+  private Course course;
+
+  @Valid
+  @NotNull
+  private Address address;
+}
